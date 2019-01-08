3077fe270123b27eda4595d218e1132c9f81016a
adolfomunoz <adolfo@unizar.es>
2019-01-08 15:24:46 +0100

    Mejorando derivada. Hay algo extraño, puede que sea en la evaluación de constantes.

--- calculus/numerical-derivative.h
+++ calculus/numerical-derivative.h
@@ -4,6 +4,8 @@ namespace sym {
 
 expression numerical_derivative(const expression& f, const symbol& x, const expression& h) {
 //	return (-f.substitute(x,x+2*h) + 8*f.substitute(x,x+h) - 8*f.substitute(x,x-h) + f.substitute(x,x-2*h))/(12*h);
+//	return (f.substitute(x,x+h) - f.substitute(x,x-h))/(2*h);
+//	These two more accurate above do not seem to work. That might be a sign that something is off.
 	return (f.substitute(x,x+h) - f)/h;
 }
 
--- main/test/derivative/derivative.cc
+++ main/test/derivative/derivative.cc
@@ -3,7 +3,13 @@
 
 template<typename A, typename B>
 void test(const sym::expression& f, const sym::symbol& x, const A& a, const B& b) {
-	std::cout<<"f = "<<f<<"\tdf/d"<<x<<" = "<<f.derivative(x)<<"\tError = "<<
+	sym::expression d1 = f.derivative(x);
+	sym::expression d2 = sym::numerical_derivative(f,x,sym::expression(1)/1000);
+	for (float t = 0.1f; t<1.0f; t+=0.1f) 
+		std::cout<<t<<"\t"<<d1.substitute(x,t)<<"\t"<<d2.substitute(x,t)<<std::endl;
+
+
+	std::cout<<"f = "<<f<<"\tdf/d"<<x<<" = "<<f.derivative(x)<<" "<<sym::numerical_derivative(f,x,sym::expression(1)/1000)<<"\tError = "<<
 		sym::integral(sym::pow(f.derivative(x) - sym::numerical_derivative(f,x,sym::expression(1)/1000),2.0f),x,a,b)<<std::endl;
 }
 
@@ -14,4 +20,12 @@ int main(int argc, char** argv) {
 	test(x*x*x,x,sym::minus_infinity,sym::infinity);
 	test(1/x,x,0.001,sym::infinity);
 	test(sym::ln(x),x,0.001,sym::infinity);
+	test(sym::log(x,10),x,0.1,0.9);
+	test(sym::pow(sym::e,x),x,sym::minus_infinity, sym::infinity);
+	test(sym::sin(x),x,sym::minus_infinity, sym::infinity);
+	test(sym::cos(x),x,sym::minus_infinity, sym::infinity);
+	test(sym::asin(x),x,-0.999, 0.999);
+	test(sym::acos(x),x,-0.999, 0.999);
+	test(3*sym::sin(sym::ln(x)),x,0.001, sym::infinity);
+	
 }
