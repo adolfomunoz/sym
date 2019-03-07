47929f11bdd5ae87d305d9397ddcfd64b87e1b3e
Adolfo <adolfo@unizar.es>
2019-03-07 18:16:35 +0100

    Added some missing pragma once and inline.

--- calculus/numerical-derivative.h
+++ calculus/numerical-derivative.h
@@ -1,8 +1,9 @@
+#pragma once
 #include "../core/expression.h"
 
 namespace sym {
 
-expression numerical_derivative(const expression& f, const symbol& x, const expression& h) {
+inline expression numerical_derivative(const expression& f, const symbol& x, const expression& h) {
 //	return (-f.substitute(x,x+2*h) + 8*f.substitute(x,x+h) - 8*f.substitute(x,x-h) + f.substitute(x,x-2*h))/(12*h);
 	return (f.substitute(x,x+h) - f.substitute(x,x-h))/(2*h);
 //	return (f.substitute(x,x+h) - f)/h;
--- calculus/quadrature.h
+++ calculus/quadrature.h
@@ -1,3 +1,4 @@
+#pragma once
 #include "../core/expression.h"
 #include <random>
 
@@ -7,7 +8,7 @@ enum Infinity { infinity };
 enum MinusInfinity { minus_infinity };
 
 
-float integral(const expression& f, const symbol& x, float a, float b, unsigned int nsamples = 100) {
+inline float integral(const expression& f, const symbol& x, float a, float b, unsigned int nsamples = 100) {
 	std::random_device seed;
 	std::mt19937 random(seed());
    	std::uniform_real_distribution<float> dist(a,b);
@@ -16,19 +17,19 @@ float integral(const expression& f, const symbol& x, float a, float b, unsigned 
 	return sum/nsamples;	
 }
 
-float integral(const expression& f, const symbol& x, const MinusInfinity& a, const Infinity& b, unsigned int nsamples = 100)
+inline float integral(const expression& f, const symbol& x, const MinusInfinity& a, const Infinity& b, unsigned int nsamples = 100)
 {
 	symbol t("t");
 	return integral(f.substitute(x,t/(1-t*t))*(1+t*t)/pow(1-t*t,2),t,-0.999999f,0.999999f,nsamples);
 }
 
-float integral(const expression& f, const symbol& x, float a, const Infinity& b, unsigned int nsamples = 100)
+inline float integral(const expression& f, const symbol& x, float a, const Infinity& b, unsigned int nsamples = 100)
 {
 	symbol t("t");
 	return integral(f.substitute(x,a + t/(1-t))/pow(1-t,2),t,0,0.999999f,nsamples);
 }
 
-float integral(const expression& f, const symbol& x, const MinusInfinity& a, float b, unsigned int nsamples = 100)
+inline float integral(const expression& f, const symbol& x, const MinusInfinity& a, float b, unsigned int nsamples = 100)
 {
 	symbol t("t");
 	return integral(f.substitute(x,b - (1 - t)/t)/pow(t,2),t,0,0.999999f,nsamples);
